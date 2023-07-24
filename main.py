f854edb4a3393680a9571e8ae62e12edc4413142
DIfficult <101355829+vsdifficult@users.noreply.github.com>
2023-07-24 12:44:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from sesion.seasion import main_parser_funciotn 
+from sesion.driver_ import driver_optios 
+
+#main_parser_funciotn('hi', 'gLFyf', 'span') 
+driver_optios('https://google.com', 'h',  'gLFyf')
